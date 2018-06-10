821f7da81452bdee55e015b7b706317938e95fa9
Jacek Matczyński <jackon996@gmail.com>
2018-06-10 23:09:12 +0200

    Created getMedicationRequests and getObservations methods

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -14,10 +14,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Identifier;
-import org.hl7.fhir.dstu3.model.Observation;
-import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.*;
+import org.hl7.fhir.exceptions.FHIRException;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -105,4 +103,61 @@ public class Controller implements Initializable{
             }
         }
     }
+
+
+    public List<MedicationRequest> getMedicationRequests(String patientID){
+        Bundle bundle = client.search().forResource(MedicationRequest.class)
+                .where(new ReferenceClientParam("subject").hasId(patientID)).returnBundle(Bundle.class)
+                .execute();
+        ArrayList<MedicationRequest> medicationRequests = new ArrayList<>();
+        while(true) {
+            List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
+            for (Bundle.BundleEntryComponent entry : entries) {
+                if (entry.getResource() instanceof MedicationRequest) {
+                    MedicationRequest medicationRequest = (MedicationRequest) entry.getResource();
+
+
+                    //Println
+                    try {
+                        System.out.println(medicationRequest.getMedicationCodeableConcept().getText());
+                    } catch (FHIRException e){
+                        e.printStackTrace();
+                    }
+
+
+                    medicationRequests.add(medicationRequest);
+                }
+            }
+            if (bundle.getLink(Bundle.LINK_NEXT) != null) {
+                bundle = client.loadPage().next(bundle).execute();
+            } else {
+                break;
+            }
+        }
+        return medicationRequests;
+    }
+
+    public List<Observation> getObservations(String patientID){
+        ArrayList<Observation> observations = new ArrayList<>();
+        Bundle bundle = client.search().forResource(Observation.class)
+                .where(new ReferenceClientParam("patient").hasId(patientID)).returnBundle(Bundle.class)
+                .execute();
+        while(true) {
+            List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
+            for (Bundle.BundleEntryComponent entry : entries) {
+                if (entry.getResource() instanceof Observation) {
+                    Observation observation = (Observation) entry.getResource();
+                    System.out.println(observation.getCode().getText());
+                    observations.add(observation);
+                }
+            }
+            if (bundle.getLink(Bundle.LINK_NEXT) != null) {
+                bundle = client.loadPage().next(bundle).execute();
+            } else {
+                break;
+            }
+        }
+        return observations;
+    }
+
 }
