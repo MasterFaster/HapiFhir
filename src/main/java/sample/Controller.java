8a401a2ed040b34e0067f00abe574471a448f683
Jacek Matczyński <jackon996@gmail.com>
2018-06-10 21:12:57 +0200

    Created very simple GUI and getPatient method

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,15 +1,22 @@
 package sample;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
+import ca.uhn.fhir.rest.gclient.StringClientParam;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.Patient;
 
 import java.net.URL;
@@ -22,11 +29,64 @@ public class Controller implements Initializable{
     private FhirContext ctx;
     private IGenericClient client;
 
+    @FXML
+    private TextField nameTextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources){
         System.out.println("HEllo world");
         ctx = new FhirContext().forDstu3();
         client = ctx.newRestfulGenericClient("http://localhost:8080/baseDstu3/");
+    }
+
+    @FXML
+    public void getPatient(){
+        String name = nameTextField.getText();
+        Bundle bundle = client.search().forResource(Patient.class)
+                .where(new StringClientParam("name").matches().value(name))
+                .returnBundle(Bundle.class).execute();
+        ArrayList<Patient> patients = new ArrayList<Patient>();
+        while(true) {
+            List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
+            for (Bundle.BundleEntryComponent entry : entries) {
+                if (entry.getResource() instanceof Patient) {
+                    Patient patient = (Patient) entry.getResource();
+                    System.out.println(patient.getName().get(0).getFamily());
+                    patients.add(patient);
+                }
+            }
+            if (bundle.getLink(Bundle.LINK_NEXT) != null) {
+                bundle = client.loadPage().next(bundle).execute();
+            } else {
+                break;
+            }
+        }
+        if (patients.size()==1){
+            System.out.println("\nObservations:");
+            String identifier = patients.get(0).getId().split("/")[5];
+            System.out.println(identifier);
+            bundle = client.search().forResource(Observation.class)
+                    .where(new ReferenceClientParam("patient").hasId(identifier)).returnBundle(Bundle.class)
+                    .execute();
+            while(true) {
+                List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
+                for (Bundle.BundleEntryComponent entry : entries) {
+                    if (entry.getResource() instanceof Observation) {
+                        Observation observation = (Observation) entry.getResource();
+                        System.out.println(observation.getCode().getText());
+                    }
+                }
+                if (bundle.getLink(Bundle.LINK_NEXT) != null) {
+                    bundle = client.loadPage().next(bundle).execute();
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    @FXML
+    public void listPatients(){
         Bundle bundle = client.search().forResource(Patient.class).returnBundle(Bundle.class).execute();
         ArrayList<Patient> patients = new ArrayList<Patient>();
         while(true) {
@@ -38,12 +98,11 @@ public class Controller implements Initializable{
                     patients.add(patient);
                 }
             }
-            if(bundle.getLink(Bundle.LINK_NEXT) != null) {
+            if (bundle.getLink(Bundle.LINK_NEXT) != null) {
                 bundle = client.loadPage().next(bundle).execute();
-            }else{
+            } else {
                 break;
             }
         }
-
     }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +17,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
