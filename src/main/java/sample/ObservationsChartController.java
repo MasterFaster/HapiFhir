ab5f6f51b9216619fd1417f9deace4adae0811da
Jacek Matczyński <jackon996@gmail.com>
2018-06-11 23:14:15 +0200

    Periods added to observations charts

--- src/main/java/sample/ObservationsChartController.java
+++ src/main/java/sample/ObservationsChartController.java
@@ -3,6 +3,9 @@ package sample;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
 import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +13,7 @@ import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.Patient;
@@ -24,13 +28,29 @@ public class ObservationsChartController implements Initializable {
 
     private String patientID;
     private String observationCode;
-    @FXML LineChart<Number, Number> observationsChart;
+    @FXML private LineChart<Number, Number> observationsChart;
+    @FXML private ChoiceBox periodChoiceBox;
+    private IGenericClient client;
 
-    public IGenericClient getClient() {
-        return client;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        observationsChart.setAnimated(false);
+        periodChoiceBox.getItems().addAll(FXCollections
+                .observableArrayList("All", "Last year", "Last month", "Last week"));
+        periodChoiceBox.getSelectionModel().selectFirst();
+        periodChoiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                showChart(newValue.intValue());
+            }
+        });
+        showChart(0);
+
     }
 
-    private IGenericClient client;
+    private IGenericClient getClient() {
+        return client;
+    }
 
     public void setPatient(String patient) {
         this.patientID = patient;
@@ -44,11 +64,9 @@ public class ObservationsChartController implements Initializable {
         this.client = client;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+
+    private void showChart(int period){
         ArrayList<Observation> observations = new ArrayList<>();
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
         Bundle bundle = client.search().forResource(Observation.class)
                 .where(new ReferenceClientParam("patient").hasId(patientID))
                 .where(new TokenClientParam("code").exactly().code(observationCode)).returnBundle(Bundle.class)
@@ -67,43 +85,76 @@ public class ObservationsChartController implements Initializable {
                 break;
             }
         }
-        Collections.sort(observations, Comparator.comparing(Observation::getIssued));
-        SimpleDateFormat dateFormatTime = new SimpleDateFormat("dd-MM-yyyy HH:MM:SS");
-        System.out.println(observationCode);
-        if (!observationCode.contains("55284-4")){
-            XYChart.Series series = new XYChart.Series<Number, Number>();
 
-            for (Observation o :
-                    observations) {
-                try{
-                    series.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
-                            o.getValueQuantity().getValue()));
-                } catch (FHIRException ex){
-                    ex.printStackTrace();
-                }
-            }
-            series.setName(observations.get(0).getCode().getText());
-            observationsChart.getData().add(series);
-        } else {
-            XYChart.Series series = new XYChart.Series<Number, Number>();
-            XYChart.Series series2 = new XYChart.Series<Number, Number>();
-
-            for (Observation o :
-                    observations) {
-                try{
-                    series.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
-                            o.getComponent().get(0).getValueQuantity().getValue()));
-                    series2.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
-                            o.getComponent().get(1).getValueQuantity().getValue()));
-                } catch (FHIRException ex){
-                    ex.printStackTrace();
-                }
-            }
-            series.setName(observations.get(0).getComponent().get(0).getCode().getText());
-            series2.setName(observations.get(0).getComponent().get(1).getCode().getText());
-            observationsChart.getData().add(series);
-            observationsChart.getData().add(series2);
+        if (period == 1){
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.YEAR, -1);
+            Date yearAgo = cal.getTime();
+            observations.removeIf(p -> p.getIssued().before(yearAgo));
+        } else if (period == 2){
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.MONTH, -1);
+            Date monthAgo = cal.getTime();
+            observations.removeIf(p -> p.getIssued().before(monthAgo));
+        } else if (period == 3){
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DAY_OF_MONTH, -7);
+            Date yearAgo = cal.getTime();
+            observations.removeIf(p -> p.getIssued().before(yearAgo));
         }
-        observationsChart.setTitle(observations.get(0).getCode().getText());
+        if(observations.size()>0){
+            Collections.sort(observations, Comparator.comparing(Observation::getIssued));
+            SimpleDateFormat dateFormatTime = new SimpleDateFormat("dd-MM-yyyy HH:MM:SS");
+            if (!observationCode.contains("55284-4")){
+                XYChart.Series series = new XYChart.Series<Number, Number>();
+                for (Observation o :
+                        observations) {
+                    try{
+                        series.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
+                                o.getValueQuantity().getValue()));
+                    } catch (FHIRException ex){
+                        ex.printStackTrace();
+                    }
+                }
+                series.setName(observations.get(0).getCode().getText());
+                observationsChart.getData().clear();
+                observationsChart.getData().addAll(series);
+                observationsChart.setTitle(observations.get(0).getCode().getText());
+                try{
+                    observationsChart.getYAxis().setLabel(observations.get(0).getValueQuantity().getUnit());
+                } catch(FHIRException ex){
+                    ex.printStackTrace();
+                }
+            } else {
+                XYChart.Series series = new XYChart.Series<Number, Number>();
+                XYChart.Series series2 = new XYChart.Series<Number, Number>();
+
+                for (Observation o :
+                        observations) {
+                    try{
+                        series.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
+                                o.getComponent().get(0).getValueQuantity().getValue()));
+                        series2.getData().add(new XYChart.Data(dateFormatTime.format(o.getIssued()),
+                                o.getComponent().get(1).getValueQuantity().getValue()));
+                    } catch (FHIRException ex){
+                        ex.printStackTrace();
+                    }
+                }
+                series.setName(observations.get(0).getComponent().get(0).getCode().getText());
+                series2.setName(observations.get(0).getComponent().get(1).getCode().getText());
+                observationsChart.getData().clear();
+                observationsChart.getData().addAll(series, series2);
+                observationsChart.setTitle(observations.get(0).getCode().getCodingFirstRep().getDisplay());
+                try {
+                    observationsChart.getYAxis().setLabel(observations.get(0).getComponentFirstRep().getValueQuantity().getUnit());
+                } catch (FHIRException ex){
+                    ex.printStackTrace();
+                }
+            }
+        } else {
+            observationsChart.getData().clear();
+
+        }
     }
+
 }
